0bd48cbdd9157fe35eda5d8680d3d2fe9d335a61
TheAshtonaught <ashtomorgan3@gmail.com>
2020-09-03 09:50:06 -0500

    Initial Commit

---
+++ gitTest2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitTest2
+//
+//  Created by Ashton Morgan on 9/3/20.
+//  Copyright © 2020 Ashton Morgan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
